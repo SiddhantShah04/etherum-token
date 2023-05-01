2eeed84a8dfc6c148a673a23d9671f208faf70a0
siddhantshah04 <siddhantshah04@gmail.com>
2023-05-01 10:39:41 +0530

    token contract commit

---
+++ modapptutsmartcontract/scripts/deploy.js
@@ -0,0 +1,37 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+
+  const [deployer] = await hre.ethers.getSigners();
+    console.log(
+      "Deploying the contracts with the account:",
+      await deployer.getAddress()
+    );
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+
+  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+
+  const Lock = await hre.ethers.getContractFactory("Token");
+  const token = await Lock.deploy();
+
+  await token.deployed();
+  console.log("Token address:", token.address);
+
+ 
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
